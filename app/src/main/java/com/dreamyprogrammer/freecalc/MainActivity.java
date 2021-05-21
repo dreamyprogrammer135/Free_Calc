d48e1abe1ac295e5ea4d729b1b9d0fd0aba5d6e2
Litvinov Ilya <dreameprogrammer82@gmail.com>
2021-05-21 19:42:51 +0300

    Сверстал экран калькулятора.

--- app/src/main/java/com/dreamyprogrammer/freecalc/MainActivity.java
+++ app/src/main/java/com/dreamyprogrammer/freecalc/MainActivity.java
@@ -3,12 +3,32 @@ package com.dreamyprogrammer.freecalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextModify;
+    private EditText editTextNotModify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setfindView();
+        setPresets();
+    }
+
+    private void setPresets() {
+        getSupportActionBar().hide();
+        editTextModify.setInputType(InputType.TYPE_NULL);
+        editTextNotModify.setInputType(InputType.TYPE_NULL);
+    }
+
+    private void setfindView() {
+        editTextModify = findViewById(R.id.et_modify);
+        editTextNotModify = findViewById(R.id.et_not_modify);
     }
 }
